a70af602d699a3fab1473b6187dea1c46f4dcc4b
dhananjayece244 <dhananjayece244@gmail.com>
2018-11-08 05:29:45 +0530

    added receiver

--- receiver.cpp
+++ receiver.cpp
@@ -5,63 +5,96 @@
 #include <stdlib.h> 
 #include <netinet/in.h> 
 #include <string.h> 
+#include "logging.h"
+#include "Message.pb.h"
 #define PORT 8080 
 
-int server_fd, new_socket, valread; 
+int server_fd, sockFd, valread,currRcvPacket,lastAckSent; 
 struct sockaddr_in address; 
 int opt = 1; 
 int addrlen = sizeof(address); 
-char buffer[1024] = {0}; 
-char *hello = "Hello from server"; 
+char buffer[1024] = {0};
+ 
+int recvPcket[1001];
+int sentAck[1001];  
+int SENT = 1;
+
+int sendPacket(int packetNum) {
+    MP::TcpMessage packet;
+    packet.set_packetnum(packetNum);
+    packet.set_msg(std::string(1000,'a'));
+    string protocolBuffer = packet.SerializeAsString();
+    int datalen = protocolBuffer.length();
+    int ret = sendto(sockFd,protocolBuffer.c_str(),datalen,0,
+                (struct sockaddr_in*)(&servAddr),sizeof(servAddr));
+                /* servAddr is global */
+    if(ret <= 0) {
+        higLog("%s","sendto() failed");
+        return FAILURE;
+    }
+    sentAck[packetNum] = true;
+    lastAckSent = packetNum;
+    return SUCCESS;
+}
+
+void timeOoutCheck() {
+    int packetNum;
+    for(packetNum = lastAckSent; packetNum<= LastRecvAck; packetNum++) {
+        if(recvPcket[packetNum] != true) {
+            LastRecvAck = packetNum-1;
+            break;   
+        }
+    }
+}
+int connectToSender(string Ip) {
+    int sockFd;
+    if ((sockFd = socket(AF_INET, SOCK_STREAM, 0)) < 0) { 
+        higLog("%s","Socket creation error");
+        return FAILURE; 
+    } 
+    memset(&servAddr, 0, sizeof(servAddr)); 
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_addr.s_addr = inet_addr(Ip.c_str());
+    servAddr.sin_port = htons(PORT); 
+    if(connect(sockFd, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0) { 
+        higLog("%s","\nConnection Failed\n");
+        return FAILURE; 
+    }
+    return sockFd;     
+}
 
 int main(int argc, char const *argv[]) 
 { 
     
-       
-    // Creating socket file descriptor 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
-    { 
-        perror("socket failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-       
-    // Forcefully attaching socket to the port 8080 
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
-                                                  &opt, sizeof(opt))) 
-    { 
-        perror("setsockopt"); 
-        exit(EXIT_FAILURE); 
-    } 
-    address.sin_family = AF_INET; 
-    address.sin_addr.s_addr = INADDR_ANY; 
-    address.sin_port = htons( PORT ); 
-       
-    // Forcefully attaching socket to the port 8080 
-    if (bind(server_fd, (struct sockaddr *)&address,  
-                                 sizeof(address))<0) 
-    { 
-        perror("bind failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-    if (listen(server_fd, 3) < 0) 
-    { 
-        perror("listen"); 
-        exit(EXIT_FAILURE); 
-    } 
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
-                       (socklen_t*)&addrlen))<0) 
-    { 
-        perror("accept"); 
-        exit(EXIT_FAILURE); 
-    } 
-    while(1){
-        valread = read( new_socket , buffer, 1024); 
-        printf("%s\n",buffer ); 
-
+    srand(time(NULL));   
+    int sockFd = connectToSender("127.0.0.1");  
+    
+    while(true){
+        memset(buffer,0,sizeof(buffer));
+        int in = recvfrom(sockFd, buffer, sizeof(buffer), 0, 
+             ( struct sockaddr *) &address, &addrlen); 
+        if(in <= 0) {
+          higLog("%s"," recvfrom() failed");
+        }
+        buffer[in] = '\0';
+        string protocolBuffer = buffer;
+        Mp::TcpMessage packet;
+        packet.ParseFromString(protocolBuffer);
+        currRcvPacket = (int)(packet.packetnum());
         
+        recvPcket[currRcvPacket] = true;
+        LastRecvAck = currRcvPacket;
 
-        send(new_socket , hello , strlen(hello) , 0 ); 
-        printf("Hello message sent\n"); 
+        if(curr-lastAckSent>threshold){
+            timeOoutCheck();
+            int ret = sendPacket(LastRecvAck);
+            if(ret == FAILURE) {
+                higLog("%s","sendPacket() failed");
+            }
+            lastAckSent = LastRecvAck; 
+        }
+          
+       
     }
     
     return 0; 
