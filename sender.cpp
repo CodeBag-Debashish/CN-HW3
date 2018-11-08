c829c20ae31af67e71accb203d7cf15fb949bc39
CodeBag-Debashish <dekadebashish3@gmail.com>
2018-11-08 09:58:24 +0530

    Added ReceiveACK function

--- sender.cpp
+++ sender.cpp
@@ -5,21 +5,22 @@
 #include <thread>
 #include <chrono>
 #include <unistd.h>
-
 #include "logging.h"
 #include "Message.pb.h"
 using namespace std;
-#define PORT 8080
-
-#define NOT_SENT    0
-#define SENT        1
-#define ACKED       2
-
+#define PORT                    8080
+#define NOT_SENT                0
+#define SENT                    1
+#define ACKED                   2
+#define MAX_BUFFER              3000
+#define MY_PORT_NUM             60000
+#define MAX_BACKLOG_REQUEST     100
+#define SLEEP_TIME              20
 const int MAX_PACKET_NUM = 10000;
-struct sockaddr_in address; 
-struct sockaddr_in servAddr;
-// global varibales
-int L, R, W;        // int is ok
+struct sockaddr_in receiverAddr; 
+struct sockaddr_in servAddr,;
+int L, R, W;
+int sockFd;
 std::chrono::milliseconds timeOut(2000);
 vector<int> status(MAX_PACKET_NUM + 1);
 vector<chrono::milliseconds> sentTime(MAX_PACKET_NUM + 1);
@@ -30,7 +31,7 @@ mutex reTransLock;
 unique_lock<mutex> windowLocker(windowLock,defer_lock);
 unique_lock<mutex> reTransLocker(reTransLock,defer_lock);
 
-// function prototypes
+
 int connectToReceiver(string);
 int sendPacket(int packetNum);
 void timeOoutCheck();
@@ -41,13 +42,17 @@ auto startTime = chrono::high_resolution_clock::now();
 
 int main(int argc, char const *argv[])  {  
     srand(time(NULL));   
-    int sockFd = connectToReceiver("127.0.0.1");
+    sockFd = connectToReceiver("127.0.0.1");
     windowLocker.lock();
     L = 1;
     R = 1;
     W = 1;
     windowLocker.unlock();
     int cnt = 1;
+
+    std::thread T1(timeOutCheck);   T1.detach();
+    std::thread T2(ReceiveACK);     T2.detach();
+
     while(true) {
         int packetNum = cnt;
         locker.lock();
@@ -102,6 +107,9 @@ int connectToReceiver(string Ip) {
     }
     return sockFd;     
 }
+/*
+this function
+*/
 int sendPacket(int packetNum) {
     int prob = rand(100) + 1;
     if(prob > 10) {
@@ -124,75 +132,91 @@ int sendPacket(int packetNum) {
     }else {
         // dont send this packet
     }
-    status[packetNum] = SENT;   // set this irrespective of sent of not sent
+    status[packetNum] = SENT;
     return SUCCESS;
 }
 
-void timeOoutCheck() {
-    std::this_thread::sleep_for(std::chrono::milliseconds(20));
+void timeOutCheck() {
+    std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP_TIME));
     while(true) {
-        auto startTime = chrono::high_resolution_clock::now();
+        auto currTime = chrono::high_resolution_clock::now();
         unique_lock<mutex> locker(windowLock,defer_lock);
         windowLocker.lock();
         for(int packetNum = L; packetNum<= R; packetNum++) {
+            auto elapsedtime = chrono::            
+            duration_cast<chrono::milliseconds>(currTime - startTime).count();
             if(status[packetNum] == SENT and 
-                currTime - sentTime[packetNum] > timeOut.count()) {
+                chrono::milliseconds(elapsedtime).count() - sentTime[packetNum]
+                .count() > timeOut.count() ) {
                 
-                R = L = packetNum;
+                L = R = packetNum;
                 W = 1;
                 reTransmit[packetNum] = true;
                 break;
-            
             }
         }
-
-
-
-
-
-
-
-
-
         windowLocker.unlock();
-        std::this_thread::sleep_for(std::chrono::milliseconds(20));
+        std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP_TIME));
     }
 }
 
-
-/* void TimeOutChecker(){
-    while(1){
-        int currTine = Time();
-        for(int i=li;i<ri;i++){
-            if(currTine-sendTime[i]>threshold){
-                Ri = Li = i;
-                windoSize = 1;
-                //send packet i
-        }
-    }
-    int x = 20000;
-    Sleep(x);
-
-    }
-
-}
-
-void ReceiveACK(){
+void ReceiveACK() {
     string recv;
+    int cnt = 0;
+    char buffer[MAX_BUFFER];
+    int addrLen = sizeof(receiverAddr);
+    int currReceivedAck;
+    int lastReceivedAck = 0;
+    int sampleRTT;
+    int estimatedRTT;
+    int deviation = 0;
+    while(true) {
+        memset(buffer,0,sizeof(buffer));
+        int in = recvfrom(sockFd, buffer, sizeof(buffer), 0, 
+                        ( struct sockaddr *) &receiverAddr, &addrlen); 
+        if(in <= 0) {
+            higLog("%s"," recvfrom() failed");
+        }
 
-    while(1){
-        char buffer[1024] = {0};
-        recv = read( sock , buffer, 1024);
-         
-        printf("%s\n",buffer ); 
+        buffer[in] = '\0';
+        string protocolBuffer = buffer;
+        Mp::TcpMessage packet;
+        packet.ParseFromString(protocolBuffer);
+        currReceivedAck = (int)(packet.packetnum());
 
+        auto currTime = chrono::high_resolution_clock::now();
+        auto elapsedtime = chrono::
+            duration_cast<chrono::milliseconds>(currTime - startTime).count();
+
+        sampleRTT = chrono::milliseconds(elapsedtime).count() 
+                    - sentTime[currReceivedAck].count();
+        if(flag == false) {
+            estimatedRTT = sampleRTT << 3;
+            flag = true;
+        }
+        sampleRTT -= (estimatedRTT >> 3);
+        estimatedRTT += sampleRTT;
+        if (sampleRTT < 0)
+            sampleRTT = -sampleRTT;
+        sampleRTT -= (deviation >> 3);
+        deviation += sampleRTT;
+        timeOut = chrono::milliseconds((estimatedRTT >> 3) + (deviation >> 1));
+
+        int acketPackets = currReceivedAck - lastReceivedAck - 1;
+        for(int packetNum = lastReceivedAck + 1; packetNum<=currReceivedAck; 
+            packetNum++ ) {
+            status[packetNum] = ACKED;
+        }
+
+        lastReceivedAck = currReceivedAck;
+        windowLocker.lock();
+        L = lastReceivedAck + 1;
+        W = W + (1.0/W) * (acketPackets);
+        R = L + floor(W); 
+        windowLocker.unlock();
+        cnt = currReceivedAck - 1;  // this will go max upto 10000
+        if(cnt == MAX_PACKET_NUM) {
+            break;
+        }    
     }
-    // temp = curent time
-    // decode the buffer message and get the ACK num = n
-    // change the left window variable Li
-    // update the window size variable
-    //update the Ri 
-    //Update the time-out value
-    //now go for sending next packet
-
-} */
+} 
